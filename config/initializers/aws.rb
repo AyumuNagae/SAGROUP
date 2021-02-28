e75706e87a0626295cb6cbeac1608d48934ff27a
Ayumu Nagae <ayumu100811@gmail.com>
2021-02-28 13:30:04 +0900

    herokuS3挑戦中

--- config/initializers/aws.rb
+++
@@ -1,6 +0,0 @@
-Aws.config.update({
-  region: 'ap-northeast-1',
-  credentials: Aws::Credentials.new('xxxxxxxxxxxxxxxx', 'xxxxxxxxxxxxxxxx'),
-})
-
-S3_BUCKET = Aws::S3::Resource.new.bucket('xxxxxxxxxxxxxxxx')
